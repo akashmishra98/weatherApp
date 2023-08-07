27ae063717d4a055da7bf87a28a88f8039a2b778
akashmishra98 <83551583+akashmishra98@users.noreply.github.com>
2023-08-08 01:42:35 +0530

    weather app

---
+++ weather.js
@@ -0,0 +1,29 @@
+
+    var apiKey ="&appid=c57607fe8dd87e056e60ea02f8e7363d";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+    
+    // var city = "goa";
+    // console.log(city);
+    // callApi();
+
+
+async function callApi()
+{
+    var city = document.getElementById('searchInput').value ;
+    var result =  await fetch(apiUrl+city+apiKey)
+    result = await result.json();
+    console.log(result);
+    document.getElementById('tempvalue').innerHTML = [result].map((data)=>{
+        let t = ((data.main.temp) - 273).toFixed(2);
+        return `${t}°C` 
+    })
+    document.getElementById('humidityval').innerHTML = [result].map((data)=>{
+        let t = ((data.main.humidity)).toFixed(2);
+        return `${t}%` 
+    })
+    document.getElementById('windVal').innerHTML = [result].map((data)=>{
+        let t = ((data.wind.speed)).toFixed(2);
+        return `${t}m/s` 
+    })
+    document.getElementById('cityname').innerHTML = city;
+}
